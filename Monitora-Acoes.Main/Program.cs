6b52b5fa15488027a7878f0c762410070c5cd547
rslewenstein <rafasoaresjf@gmail.com>
2021-09-13 20:32:49 -0300

    first commit

---
+++ Monitora-Acoes.Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Monitora_Acoes.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Monitora Ações
+
+### PT-BR
+Bot para monitorar os preções das ações da bolsa brasileira.
+
+### EN
+Bot to monitor the stocks prices of the Brazilian stock exchange.
