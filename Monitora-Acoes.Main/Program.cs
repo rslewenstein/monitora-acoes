f9e3aabaa7c8403517a35b6fe785450548c6ddc7
rslewenstein <rafasoaresjf@gmail.com>
2021-09-14 22:54:04 -0300

    passing data byparam

--- Monitora-Acoes.Main/Program.cs
+++ Monitora-Acoes.Main/Program.cs
@@ -15,8 +15,27 @@ namespace Monitora_Acoes.Main
             // JobManager.Initialize(new ConfigurationCrawler());
             // Console.ReadLine();
 
-            var gdc = new GetDataCrawler();
-            gdc.Execute();
+            //string stocks = "petr3, aaaa2, taee4, itsa4";
+            string stocks = null;
+            Console.WriteLine("Digite os códigos separados por vírgula. \nEx: abcd1, efgh2 \nou digite apenas um código. \nEx: bbbb3");
+            Console.WriteLine("");
+            stocks = Console.ReadLine();
+
+            while (string.IsNullOrEmpty(stocks))
+            {
+                Console.WriteLine("Digite algum código para pesquisa.");
+                Console.WriteLine("");
+                Console.WriteLine("Digite os códigos separados por vírgula. \nEx: abcd1, efgh2 \nou digite apenas um código. \nEx: bbbb3");
+                Console.WriteLine("");
+                stocks = Console.ReadLine();
+            }
+
+
+            if (!string.IsNullOrEmpty(stocks))
+            {
+                var gdc = new GetDataCrawler();
+                gdc.Execute(stocks);
+            }
         }
     }
 }
