a22773b4e3ea1412e7b27dafa50e83dfa75f585a
rslewenstein <rafasoaresjf@gmail.com>
2021-09-14 23:36:52 -0300

    add sleep

--- Monitora-Acoes.Main/Program.cs
+++ Monitora-Acoes.Main/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using FluentScheduler;
 using Monitora_Acoes.Crawler;
 
@@ -8,24 +9,17 @@ namespace Monitora_Acoes.Main
     {
         static void Main(string[] args)
         {
-            // Deverá receber um ou mais códigos de ações e pesquisar.
-            // Talvez será necessário utilizar o Sleep
-            // e chamar o método direto na classe GetDataCrawler
-
             // JobManager.Initialize(new ConfigurationCrawler());
             // Console.ReadLine();
 
             //string stocks = "petr3, aaaa2, taee4, itsa4";
-            string stocks = null;
             Console.WriteLine("Digite os códigos separados por vírgula. \nEx: abcd1, efgh2 \nou digite apenas um código. \nEx: bbbb3");
             Console.WriteLine("");
-            stocks = Console.ReadLine();
+            var stocks = Console.ReadLine();
 
             while (string.IsNullOrEmpty(stocks))
             {
                 Console.WriteLine("Digite algum código para pesquisa.");
-                Console.WriteLine("");
-                Console.WriteLine("Digite os códigos separados por vírgula. \nEx: abcd1, efgh2 \nou digite apenas um código. \nEx: bbbb3");
                 Console.WriteLine("");
                 stocks = Console.ReadLine();
             }
@@ -34,7 +28,22 @@ namespace Monitora_Acoes.Main
             if (!string.IsNullOrEmpty(stocks))
             {
                 var gdc = new GetDataCrawler();
-                gdc.Execute(stocks);
+                int cont = 1;
+
+                while (cont > 0)
+                {
+                    try
+                    {
+                        gdc.Execute(stocks);
+                        Thread.Sleep(30000);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Ops... " + ex.ToString());
+                        cont = 0;
+                    }
+                }
+
             }
         }
     }
