d0015d8e783d988787fa4fe3718fda4e0ea260fd
rslewenstein <rafasoaresjf@gmail.com>
2021-09-25 12:11:25 -0300

    add new project

--- Monitora-Acoes.Main/Program.cs
+++ Monitora-Acoes.Main/Program.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
+using System.IO;
 using Telegram.Bot;
 using FluentScheduler;
-using Monitora_Acoes.Crawler;
-using Microsoft.Extensions.Configuration.Json;
-using System.IO;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using Monitora_Acoes.Bot;
 
 namespace Monitora_Acoes.Main
 {
@@ -18,37 +10,11 @@ namespace Monitora_Acoes.Main
     {
         private static string TelegramToken = @"settingsExt.txt";
         private static string token = File.ReadAllText(TelegramToken);
-        private static string chatId = YOUR_ID;
 
         static void Main(string[] args)
         {
-            SendMessageTelegram(token, chatId);
+            var bot = new BotSendMessage();
+            bot.SendMessageTelegram(token);
         }
-
-
-        public static void SendMessageTelegram(string token, string chatId)
-        {
-            string apiToken = token;
-            List<string> stocks = ProcessMessage(chatId);
-
-            foreach (var stock in stocks)
-            {
-                string urlString = "https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}";
-                urlString = String.Format(urlString, apiToken, chatId, stock);
-                WebRequest request = WebRequest.Create(urlString);
-                Stream rs = request.GetResponse().GetResponseStream();
-            }
-        }
-
-        public static List<string> ProcessMessage(string chatId)
-        {
-            var stocks = "petr3, taee4, aaaa2, sapr3";
-            var gdc = new GetTextCrawler();
-            List<string> retStocks = new List<string>();
-            String IdAux = chatId;
-            retStocks = gdc.Execute(stocks);
-            return retStocks;
-        }
-
     }
 }
